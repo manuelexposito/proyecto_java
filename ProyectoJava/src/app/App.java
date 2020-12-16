794d736f99636e71116ef4761e399b92026ff7c2
Manuel Exposito <manuelexposito4d@gmail.com>
2020-12-16 10:42:12 +0100

    Añadido leer por teclado y saludar al usuario

--- ProyectoJava/src/app/App.java
+++ ProyectoJava/src/app/App.java
@@ -6,9 +6,9 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo");
-		
-		
+		System.out.println("Introduce tu nombre:");
+		String nombre = leerStringPorConsola();
+		System.out.println("Hola, "+nombre);
 		
 		
 	}
