16470d66cb69f7fb04f3da1e3273eeecea348337
Manuel Exposito <manuelexposito4d@gmail.com>
2020-12-16 10:38:56 +0100

    Leer por teclado

--- ProyectoJava/src/app/App.java
+++ ProyectoJava/src/app/App.java
@@ -1,11 +1,25 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo");
 		
+		
+		
+		
+	}
+	
+	
+	public static String leerStringPorConsola() {
+		
+		Scanner sc = new Scanner(System.in);
+		String result = sc.next();
+		sc.close();
+		return result;
 		
 		
 		
