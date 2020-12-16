8ff8894eedd492332f1e646db26b56c295bc0227
Manuel Exposito <manuelexposito4d@gmail.com>
2020-12-16 10:50:21 +0100

    Funcion de saludar finalizada

--- ProyectoJava/src/app/App.java
+++ ProyectoJava/src/app/App.java
@@ -8,7 +8,7 @@ public class App {
 		
 		System.out.println("Introduce tu nombre:");
 		String nombre = leerStringPorConsola();
-		System.out.println("Hola, "+nombre);
+		System.out.println("HOLA, "+nombre.toUpperCase());
 		
 		
 	}
